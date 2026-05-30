da7008595f2611435a8dd12d76cf27616097e151
Savan Patel <srspco2020@gmail.com>
2026-05-30 19:45:50 +0530

    Added Railway new

--- gst_project/settings.py
+++ gst_project/settings.py
@@ -85,8 +85,13 @@ WSGI_APPLICATION = 'gst_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
+
+
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
